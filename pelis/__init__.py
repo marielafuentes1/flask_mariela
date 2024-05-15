ca06b4ea0f67050ae98349f5f30177b8a737c1e1
mariela <mf44875939@gmail.com>
2024-05-15 11:16:54 -0300

    realize la pagina con las categorias de la pelicula

--- pelis/__init__.py
+++ pelis/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -10,7 +10,17 @@ with app.app_context():
 def hello():
     return 'Hello, World!'
 
-@app.route('/quien')
-def quien():
-    return 'echo por mariela fuentes'
+
+@app.route("/category")
+def category():
+   consulta = """
+       SELECT name FROM category
+       ORDER BY name;
+   """
+   con = db.get_db()
+   res = con.execute(consulta)
+   lista_category = res.fetchall()
+   pagina = render_template("category.html", categorias = lista_category)
+  
+   return pagina
 
